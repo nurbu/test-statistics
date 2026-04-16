c0518459b3c166864776e79cf868eecc5c9f99ca
Nurbu <lamanurbu2@gmail.com>
2026-04-16 15:15:12 -0400

    Created a method to get average test score.

--- src/main/java/com/pluralsight/TestScores.java
+++ src/main/java/com/pluralsight/TestScores.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
 public class TestScores {
+    public static void main(String[] args) {
+
+        double testScores[] = {50.2, 100.2, 75, 95, 75, 50, 20, 50, 90, 80};
+
+        System.out.println("Test scores: " + averageScore(testScores));
+
+    }
+
+    // Calculates average
+    public static double averageScore(double testScores[]) {
+
+        double totalScore = 0;
+
+        for (int i = 0; i < testScores.length; i++) {
+            totalScore += testScores[i];
+        }
+
+        double average = totalScore / testScores.length;
+
+        return average;
+    }
 }
